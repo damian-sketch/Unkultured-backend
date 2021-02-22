4ac5e05db4be7a4d02e5bd698b39577dd19bad83
damian-sketch <cedricdamian19@gmail.com>
2021-02-23 02:40:08 +0300

    Add functions to get posts of different categories

--- controllers/postController.js
+++ controllers/postController.js
@@ -6,7 +6,7 @@ async function allPosts(req, res) {
    const posts = await Posts.find({});
    console.log(posts);
    res.send(posts);  
-}
+};
 
 
 // Get all posts under 'Covers category'
@@ -15,16 +15,59 @@ async function coverPosts(req, res) {
    const posts = await Posts.find(covers);
    console.log(posts);
    res.send(posts);  
-}
+};
 
 
-// Get all posts under 'style category'
+// Get all posts under 'Style category'
 const style = { "category": "style" };
 async function stylePosts(req, res) {
     const posts = await Posts.find(style);
     console.log(posts);
     res.send(posts);  
- }
+ };
 
+ // Get all posts under 'Culture category'
+const culture = { "category": "culture" };
+async function culturePosts(req, res) {
+    const posts = await Posts.find(culture);
+    console.log(posts);
+    res.send(posts);  
+ };
 
-export { coverPosts, stylePosts, allPosts };
+ // Get all posts under 'Music category'
+ const music = { "category": "music" };
+ async function musicPosts(req, res) {
+     const posts = await Posts.find(music);
+     console.log(posts);
+     res.send(posts);  
+  };
+
+   // Get all posts under 'Unkultured TV category'
+ const videos = { "category": "videos" };
+ async function videoPosts(req, res) {
+     const posts = await Posts.find(videos);
+     console.log(posts);
+     res.send(posts);  
+  };
+
+  // Create a post
+async function createPost(req, res) {
+   console.log(req);
+   try {
+       if (!req.body.title && !req.body.content && !req.body.category) {
+         console.log('There is an error')  
+         return res.status(403).end();
+           
+       }
+
+       const newPost = new Posts(req.body);
+       const post = await newPost.save();
+       res.status(201).json(post);
+       
+   }
+   catch (err) {
+   console.log(err);
+   }
+}
+
+export { coverPosts, stylePosts, allPosts, culturePosts, musicPosts, videoPosts, createPost }
--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { coverPosts, stylePosts, allPosts } from '../controllers/postController.js';
+import { coverPosts, stylePosts, allPosts, culturePosts, musicPosts, videoPosts, createPost } from '../controllers/postController.js';
 
 const router = express.Router();
 
@@ -7,6 +7,12 @@ const router = express.Router();
 router.get('/', allPosts)
       .get('/covers', coverPosts)
       .get('/style', stylePosts)
+      .get('/culture', culturePosts)
+      .get('/music', musicPosts)
+      .get('/videos', videoPosts)
+
+// Create a post
+router.post('/create-post', createPost)      
 
 
 export default router;
